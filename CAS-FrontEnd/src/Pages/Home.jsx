49fecfd4b722fd98c556a72df2a6dbc8e6771864
Ashwinumbarkar20 <Ashwinumbarkar20@gmail.com>
2023-12-27 12:24:04 +0530

    Hero-section

--- CAS-FrontEnd/src/Pages/Home.jsx
+++ CAS-FrontEnd/src/Pages/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import Heroimg from '../assets/Hero.png'
 export default function Home() {
   return (
-    <div>
-      In Home page
-    </div>
+    <HeroSection>
+      <div className='container-fluid'>
+<img src={Heroimg} className='Hero-image img-fluid' alt="Heroimage"/>
+<h1 className="Hero-heading">Lorem ipsum dolor  amet consectetur, adipisicing elit. Aliquam eligendi quam eum quo nihil, quos, dolor sit amet commodi consectetur,commodi maiores fuga, rem  .</h1>
+      </div>
+    </HeroSection>
   )
 }
+const HeroSection =styled.section`
+.container-fluid{
+  position:relative;
+}
+.Hero-image{
+  
+  width:100%;
+  object-fit:cover;
+  height:70vh;
+  filter: brightness(40%) blur(6px);
+ 
+  
+  
+}
+.Hero-heading{
+  width:90%;
+  position:absolute;
+  position: absolute;
+    top: 50%; 
+    right: 50%;
+    transform: translate(50%,-50%);
+    background-color:transparent;
+    color: #fff;
+ text-align:center;
+
+}
+`
